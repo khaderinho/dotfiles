b45bc343c3bb6e316b52024e3923043a2b0f7074
Euler <amr.euler@gmail.com>
2026-09-24 19:18:22 +0300

    Update dev.sh

--- fedora_build/dev.sh
+++ fedora_build/dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo dnf group install -y "Development Tools"
+sudo dnf install -y @development-tools
 sudo dnf install -y git wget curl openssh stow opendoas
