2285e99e646656b12b7223dfa184fca3f06ea5cf
euler <amr.euler@gmail.com>
2026-09-26 20:27:51 +0300

    adding brave sync code

--- fedora_build/web.sh
+++ fedora_build/web.sh
@@ -2,3 +2,6 @@
 set -euo pipefail
 
 sudo dnf install -y firefox chromium brave-origin
+
+# Brave Code
+valve side ghost field final crouch middle lucky advice tool tooth public shadow crazy climb term push lion vivid test voice guard popular tape sight
