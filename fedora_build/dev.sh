cdb351d2745b1379b959ca4033e1dc1e2edce77f
euler <amr.euler@gmail.com>
2026-09-26 20:26:19 +0300

    Fedora Spin

--- fedora_build/dev.sh
+++ fedora_build/dev.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 sudo dnf install -y @development-tools
-sudo dnf install -y git wget curl openssh stow opendoas code
+sudo dnf install -y git wget curl openssh stow code sublime-text
--- fedora_build/repositories.sh
+++ fedora_build/repositories.sh
@@ -12,3 +12,8 @@ fi
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+## Sublime text
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+
+sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
